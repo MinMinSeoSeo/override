df4e109c0614a966477176a0fef3459386eae65e
diserpiero <kurzwk1@gmail.com>
2024-11-21 16:59:34 +0900

    css 수정 완료 (page4 기준으로 page5에 맞게 수정 -> src/page1_4.css)

--- src/page4/Page4.jsx
+++ src/page4/Page4.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./page1_4.css"; 
+import "../page1_4.css"; 
 import "./page4.css"; 
 
 const Page4 = () => {
