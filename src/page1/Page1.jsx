28d7740020aeeb29ad4464269967c5d5fc26150d
diserpiero <kurzwk1@gmail.com>
2024-11-23 16:49:48 +0900

    page1 디자인 보완 완료

--- src/page1/Page1.jsx
+++ src/page1/Page1.jsx
@@ -40,66 +40,72 @@ const Page1 = () => {
             margin: 0 25px;
             padding: 0;
             border-radius: 50%;
+            border-width: 2px;
             justify-content: center;
             transition: all 0.3s;
             background-color: white;
           }
 
+          .option:hover {
+            color: darkgreen;
+            font-size: 65px;
+            transition: all 0.3s;
+            background-color: #b7deb7;
+          }
+
           .option.selected {
-            width: 130px;
-            height: 130px;
+            width: 140px;
+            height: 140px;
+            border-width: 2px;
           }
           
           .option.selected .subtext-context{
-            color: darkgreen;
-            font-size: 50px;
-            font-weight: bold;
+            color: green;
+            font-size: 65px;
             transition: all 0.3s;
           }
           
           .subtext-context {
             padding: 0;
-            font-size: 22px;
+            font-family: 'Inter';
+            font-size: 48px;
+            color: #888;
+            transition: all 0.3s;
           }
         `}
       </style>
       <div className="progress-bar">
         <div className="progress" style={{ width: `${(1 / 5) * 100}%` }}></div>
       </div>
-      <h1>타고 싶은 놀이기구 개수를 선택해주세요. (미완) </h1>
+      <h1>타고 싶은 놀이기구 개수를 선택해주세요.</h1>
       <div className="options">
         <div id="horizontal-line"></div>
         <div
           className={`option ${selectedOption === 1 ? "selected" : ""}`}
-          onMouseEnter={() => handleSelect(1)}
           onClick={() => handleSelect(1)}
         >
           <p className="subtext-context">1</p>
         </div>
         <div
           className={`option ${selectedOption === 2 ? "selected" : ""}`}
-          onMouseEnter={() => handleSelect(2)}
           onClick={() => handleSelect(2)}
         >
           <p className="subtext-context">2</p>
         </div>
         <div
           className={`option ${selectedOption === 3 ? "selected" : ""}`}
-          onMouseEnter={() => handleSelect(3)}
           onClick={() => handleSelect(3)}
         >
           <p className="subtext-context">3</p>
         </div>
         <div
           className={`option ${selectedOption === 4 ? "selected" : ""}`}
-          onMouseEnter={() => handleSelect(4)}
           onClick={() => handleSelect(4)}
         >
           <p className="subtext-context">4</p>
         </div>
         <div
           className={`option ${selectedOption === 5 ? "selected" : ""}`}
-          onMouseEnter={() => handleSelect(5)}
           onClick={() => handleSelect(5)}
         >
           <p className="subtext-context">5</p>
