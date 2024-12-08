f44e5a411dc7742ab88fbe71874fd3089eb3bf7a
diserpiero <kurzwk1@gmail.com>
2024-12-08 17:58:48 +0900

    Result Page에서의 오류 해결 (Home 버튼이랑 Previous랑 바뀐 문제 + Home 버튼 눌렀을 때 오류 생기던 거)

--- src/api/attraction.js
+++ src/api/attraction.js
@@ -28,9 +28,9 @@ export async function recommendAttractionsApi({
     }
 
     const data = await res.json();
-
     return data;
   } catch (error) {
+
     console.error(error);
 
     return null;
